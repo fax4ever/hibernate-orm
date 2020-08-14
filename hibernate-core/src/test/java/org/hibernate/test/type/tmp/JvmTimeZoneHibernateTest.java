7eb7abe723ae3f338d3180bd8d186b4c3171c84d
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2020-08-14 09:38:17 +0200

    HHH-14031 Reproduce the issue with and without Hibernate

---
+++ hibernate-core/src/test/java/org/hibernate/test/type/tmp/JvmTimeZoneHibernateTest.java
@@ -0,0 +1,86 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or http://www.gnu.org/licenses/lgpl-2.1.html
+ */
+package org.hibernate.test.type.tmp;
+
+import java.time.LocalDate;
+import java.util.TimeZone;
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import org.hibernate.testing.junit4.BaseCoreFunctionalTestCase;
+import org.junit.Test;
+
+import org.assertj.core.api.Assertions;
+
+public class JvmTimeZoneHibernateTest extends BaseCoreFunctionalTestCase {
+
+	private static final String ENTITY_NAME = "theentity";
+
+	private final TimeZone timeZoneBefore;
+
+	public JvmTimeZoneHibernateTest() {
+		this.timeZoneBefore = TimeZone.getDefault();
+	}
+
+	@Override
+	protected final Class<?>[] getAnnotatedClasses() {
+		return new Class[] { MyEntity.class };
+	}
+
+	@Test
+	public void test_rome() {
+		withDefaultTimeZone( TimeZone.getTimeZone( "Europe/Rome" ), 1 );
+	}
+
+	@Test
+	public void test_dallas() {
+		withDefaultTimeZone( TimeZone.getTimeZone( "America/Dallas" ), 2 );
+	}
+
+	private final void withDefaultTimeZone(TimeZone timeZone, int id) {
+		TimeZone.setDefault( timeZone );
+
+		try {
+			test( id );
+		}
+		finally {
+			TimeZone.setDefault( timeZoneBefore );
+		}
+	}
+
+	private void test(int id) {
+		MyEntity entity = new MyEntity( id, LocalDate.of( 1997, 5, 5 ) );
+		inTransaction( session -> {
+			session.persist( entity );
+		} );
+
+		// reloaded from another session / transaction
+		inTransaction( session -> {
+			MyEntity reloaded = session.find( MyEntity.class, id );
+			Assertions.assertThat( reloaded.value ).isEqualTo( LocalDate.of( 1997, 5, 5 ) );
+		} );
+	}
+
+	@Entity(name = ENTITY_NAME)
+	private static final class MyEntity {
+
+		@Id
+		private Integer id;
+
+		@Basic
+		public LocalDate value;
+
+		private MyEntity() {
+		}
+
+		public MyEntity(int id, LocalDate value) {
+			this.id = id;
+			this.value = value;
+		}
+	}
+}
---
+++ hibernate-core/src/test/java/org/hibernate/test/type/tmp/JvmTimeZoneJdbcTest.java
@@ -0,0 +1,135 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or http://www.gnu.org/licenses/lgpl-2.1.html
+ */
+package org.hibernate.test.type.tmp;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.TimeZone;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import org.assertj.core.api.Assertions;
+
+public class JvmTimeZoneJdbcTest {
+
+	private static final String DB_DRIVER = "org.h2.Driver";
+	private static final String DB_CONNECTION = "jdbc:h2:~/test";
+	private static final String DB_USER = "";
+	private static final String DB_PASSWORD = "";
+
+	private static final String DROP_TABLE_SQL = "drop table if exists theentity";
+	private static final String CREATE_TABLE_SQL = "create table theentity (id integer not null, value date, primary key (id) )";
+	private static final String INSERT_SQL = "insert into theentity (value, id) values (?, ?)";
+	private static final String QUERY_SQL = "select changedefa0_.id as id1_0_0_, changedefa0_.value as value2_0_0_ from theentity changedefa0_ where changedefa0_.id=?";
+
+	private final TimeZone timeZoneBefore;
+
+	public JvmTimeZoneJdbcTest() {
+		timeZoneBefore = TimeZone.getDefault();
+	}
+
+	@Test
+	public void test_rome() {
+		try {
+			TimeZone.setDefault( TimeZone.getTimeZone( "Europe/Rome" ) );
+			insertAndGetDate( 1 );
+		}
+		finally {
+			TimeZone.setDefault( timeZoneBefore );
+		}
+	}
+
+	@Test
+	public void test_dallas() {
+		try {
+			TimeZone.setDefault( TimeZone.getTimeZone( "America/Dallas" ) );
+			insertAndGetDate( 2 );
+		}
+		finally {
+			TimeZone.setDefault( timeZoneBefore );
+		}
+	}
+
+	@Before
+	public void createTable() {
+		try (Connection connection = getConnection()) {
+			connection.setAutoCommit( false );
+
+			try (PreparedStatement preparedStatement = connection.prepareStatement( DROP_TABLE_SQL )) {
+				preparedStatement.execute();
+			}
+			try (PreparedStatement preparedStatement = connection.prepareStatement( CREATE_TABLE_SQL )) {
+				preparedStatement.execute();
+			}
+		}
+		catch (SQLException e) {
+			Assertions.fail( e.getMessage(), e );
+		}
+	}
+
+	@After
+	public void dropTable() {
+		try (Connection connection = getConnection()) {
+			connection.setAutoCommit( false );
+
+			try (PreparedStatement preparedStatement = connection.prepareStatement( DROP_TABLE_SQL )) {
+				preparedStatement.execute();
+			}
+		}
+		catch (SQLException e) {
+			Assertions.fail( e.getMessage(), e );
+		}
+	}
+
+	private void insertAndGetDate(int id) {
+		try (Connection connection = getConnection()) {
+			try (PreparedStatement preparedStatement = connection.prepareStatement( INSERT_SQL )) {
+				Date sqlDate = Date.valueOf( LocalDate.of( 1997, 5, 5 ) );
+				Assertions.assertThat( sqlDate ).hasDayOfMonth( 5 );
+
+				preparedStatement.setDate( 1, sqlDate );
+				preparedStatement.setInt( 2, id );
+				preparedStatement.executeUpdate();
+			}
+			try (PreparedStatement preparedStatement = connection.prepareStatement( QUERY_SQL )) {
+				preparedStatement.setInt( 1, id );
+				try (ResultSet resultSet = preparedStatement.executeQuery()) {
+					Assertions.assertThat( resultSet ).isNotNull();
+					Assertions.assertThat( resultSet.next() ).isTrue();
+					Assertions.assertThat( resultSet.getDate( "value2_0_0_" ) ).hasDayOfMonth( 5 );
+				}
+			}
+		}
+		catch (SQLException e) {
+			Assertions.fail( e.getMessage(), e );
+		}
+	}
+
+	private static Connection getConnection() {
+		Connection connection = null;
+		try {
+			Class.forName( DB_DRIVER );
+		}
+		catch (ClassNotFoundException e) {
+			Assertions.fail( "Driver class not found. " + e.getMessage(), e );
+		}
+		try {
+			return DriverManager.getConnection( DB_CONNECTION, DB_USER, DB_PASSWORD );
+		}
+		catch (SQLException e) {
+			Assertions.fail( "Error acquiring connection. " + e.getMessage(), e );
+		}
+		return connection;
+	}
+}
